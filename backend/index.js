0685ec0dbea1d659777dd9c0bf318bd29a549d15
girirajPanchariya <gafsa3768@gmail.com>
2026-01-22 21:42:21 +0530

    this is reday

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.use(cors({
   origin: [
     "http://localhost:5173",
